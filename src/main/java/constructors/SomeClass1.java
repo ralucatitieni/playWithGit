9640f2408606f4e1e5cf3d332f30412e8bf696df
raluca.titieni <raluca.titieni@fortech.ro>
2021-09-29 15:21:07 +0300

    Make some basic changes in SomeClass1 and SomeClass2

--- src/main/java/constructors/SomeClass1.java
+++ src/main/java/constructors/SomeClass1.java
@@ -9,6 +9,8 @@ public class SomeClass1 {
   
 int someVariable1 = 4;
 
+    int someVariable1 = 4;
+
     public boolean isDateInThePast(LocalDate localDate) {
         if (localDate.isBefore(LocalDate.now())) {
             return true;
