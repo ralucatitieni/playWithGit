c5ffd8e4ab68eb518d8c02c9ee1f7f1317b72a11
raluca.titieni <raluca.titieni@fortech.ro>
2021-09-29 15:24:54 +0300

    Add a new method to SomeClass1

--- src/main/java/constructors/SomeClass1.java
+++ src/main/java/constructors/SomeClass1.java
@@ -2,8 +2,15 @@ package constructors;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class SomeClass1 {
 
-
+    public boolean isDateInThePast(LocalDate localDate) {
+        if (localDate.isBefore(LocalDate.now())) {
+            return true;
+        }
+        return false;
+    }
 }
